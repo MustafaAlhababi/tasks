e1a9213460decf9b05f76c2f8bc66925281ce774
MustafaAlhababi <7773mustafa@gmail.com>
2023-09-05 22:36:44 +0300

    the end...

---
+++ script.js
@@ -0,0 +1,27 @@
+﻿function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+    if (taskText === "") return;
+
+    const taskList = document.getElementById("taskList");
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = 
+        "<p>"+taskText+"</p><button class='edit-button' onclick='editTask(this)'>تعديل</button><button onclick='deleteTask(this)'>حذف</button>"
+        ;
+    taskList.appendChild(card);
+
+    taskInput.value = "";
+}
+
+function editTask(button) {
+    const taskText = button.parentElement.querySelector("p").textContent;
+    const updatedTaskText = prompt("قم بتعديل المهمة:", taskText);
+    if (updatedTaskText !== null) {
+        button.parentElement.querySelector("p").textContent = updatedTaskText;
+    }
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
